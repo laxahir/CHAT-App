0091e80e6e9fcdebe4f965bf3d455b3a95c44d66
laxahir <laxahir88@gmail.com>
2025-05-12 17:41:11 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,9 @@ app.use("/api/user", userRouter);
 app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-// })
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 const PORT = process.env.PORT || 3000
 
