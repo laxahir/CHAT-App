4a46d9b24e52b41cd26d89720348607b8e0cadd8
laxahir <laxahir88@gmail.com>
2025-05-12 16:51:38 +0530

    #

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,11 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-<<<<<<< HEAD
+
 app.use(cors({
     origin: "http://localhost:5173", // frontend dev server
     credentials: true                // allow cookies
 }));
-=======
-// app.use(cors({
-//     origin: "http://localhost:5173", // frontend dev server
-//     credentials: true                // allow cookies
-// }));
->>>>>>> 29d479e691e6ef6d392cfd54755818dc38f6542c
 
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRouter);
@@ -35,15 +29,11 @@ app.use("/api/user", userRouter);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-<<<<<<< HEAD
+
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 // })
-=======
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
->>>>>>> 29d479e691e6ef6d392cfd54755818dc38f6542c
+
 
 const PORT = process.env.PORT || 3000
 
