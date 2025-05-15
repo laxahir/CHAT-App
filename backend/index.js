2dbd5b22a6885976198669a4883eabf3ac17f16a
laxahir <laxahir88@gmail.com>
2025-05-15 17:30:19 +0530

    #

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,6 @@ app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname,
         "..", "frontend", "dist", "index.html"));
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 
