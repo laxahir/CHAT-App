a9d709f85f1810009c9ca756dd28d13403c3835b
laxahir <laxahir88@gmail.com>
2025-07-31 12:33:49 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,28 @@
+// import { defineConfig } from 'vite';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss()
+//   ],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3000",
+//         secure: false
+//       }
+//     }
+//   }
+// });
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
+  base: '/', // ensures correct routing on Netlify
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3000", // your backend
+        changeOrigin: true,
         secure: false
       }
     }
